52eff4d93c657e7025c9bf1ad484d9669a0fa328
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-02-05 21:55:42 +0800

    Update json_demo_db.php

--- json_demo_db.php
+++ json_demo_db.php
@@ -2,9 +2,9 @@
 header("Content-Type: application/json; charset=UTF-8");
 $obj = json_decode($_GET["x"], false);
 
-$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
-$stmt = $conn->prepare("SELECT name FROM ? LIMIT ?");
-$stmt->bind_param("ss", $obj->table, $obj->limit);
+$conn = new mysqli("localhost", "iberkatm", "shati5620", "iberkatm_iberkat");
+$stmt = $conn->prepare("SELECT ? FROM ? LIMIT ?");
+$stmt->bind_param("ss", $obj->field, $obj->table, $obj->limit);
 $stmt->execute();
 $result = $stmt->get_result();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
