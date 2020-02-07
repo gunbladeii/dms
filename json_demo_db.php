cec5e8ff4900cef51ce3033f69fb723aa19befef
mohd fisha hafiz bin abidin <fisha.hafiz@moe.gov.my>
2020-02-07 15:17:03 +0800

    Update json_demo_db.php

--- json_demo_db.php
+++ json_demo_db.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json; charset=UTF-8");
 $obj = json_decode($_GET["x"], false);
 
-$conn = new mysqli("localhost", "	u846469917_iberkat", "shati5620", "u846469917_dms");
+$conn = new mysqli("localhost", "u846469917_iberkat", "shati5620", "u846469917_dms");
 $stmt = $conn->prepare("SELECT ? FROM ? LIMIT ?");
 $stmt->bind_param("sss", $obj->field, $obj->table, $obj->limit);
 $stmt->execute();
